b3c668c6ed0d0c6fd7e1bc9eda4a2b551db2c5e3
CiscoGalvan <fragalva@ucm.es>
2024-03-15 12:34:08 +0100

    mas cambikos

---
+++ src/Components/MovementComponent.cpp
@@ -0,0 +1,13 @@
+//#include "MovementComponent.h"
+//#include "GuilleElArquitecto/TransformComponent.h"
+//#include "GuilleElArquitecto/Entity.h"
+//
+//void VeryReal::MovementComponent::InitComponent(){
+//
+//	my_transform = this->GetEntity()->GetComponent<TransformComponent>("TransformComponent");
+//}
+//
+//void VeryReal::MovementComponent::LateUpdate(const double& dt) {
+//	if (IsMoving())
+//		my_transform->Translate(movementDirection * speed * dt);
+//}
---
+++ src/Components/PlayerInputComponent.h
@@ -0,0 +1,27 @@
+//#pragma once
+//#pragma once
+//#ifndef INPUTCOMPONENT
+//#define INPUTCOMPONENT
+//#include "GuilleElArquitecto/Component.h"
+//#include "GuilleElArquitecto/Vector3.h"
+//#include "GuilleElArquitecto/CreatorComponent.h"
+//
+//namespace VeryReal {
+//	class MovementComponent;
+//}
+//
+//namespace VeryReal {
+//	class PlayerInputComponent : public Component {
+//	public:
+//		virtual void InitComponent();
+//		virtual void Update(const double& dt);
+//
+//		inline bool IsFlashLightPressed() { return flashlight; }
+//
+//	private:
+//		bool flashlight;
+//
+//		MovementComponent* my_movement_component;
+//	};
+//}
+//#endif
