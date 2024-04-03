00092076560cbf6b5b0b1732f92531047e48d519
Cristina Mora <Crmora03@ucm.es>
2024-04-03 14:21:48 +0200

    merodeo
    no lo puedo comprobar porque no compila

---
+++ projects/MerodeoMovementComponent.cpp
@@ -0,0 +1,44 @@
+#include "MerodeoMovementComponent.h"
+#include "RigidBodyComponent.h"
+#include "TransformComponent.h"
+#include "Entity.h"
+#include <cstdlib>
+MerodeoMovementComponent::MerodeoMovementComponent() {
+	transfrormcomponent = this->GetEntity()->GetComponent<VeryReal::TransformComponent>("TransformComponent");
+	rigidbodycomponent = this->GetEntity()->GetComponent<VeryReal::RigidBodyComponent>("RigidBodyComponent");
+}
+MerodeoMovementComponent::~MerodeoMovementComponent() {
+
+}
+int MerodeoMovementComponent::random360() {
+	int random = rand() % 361;
+	int signo = rand() % 2;
+	if (signo == 0) {
+		return random;
+	}
+	else return -random;
+}
+void MerodeoMovementComponent::Update(const double& dt){
+	if (timertochange < actualtimer) {
+		//si mi tiempo actual es mayor que cuando deberia cambiar entonces actualizo direcciones
+		
+		
+		Vector3 fDirection = transfrormcomponent->GetPosition();
+		Vector3 bdirection = Vector3(-fDirection.GetX(), -fDirection.GetY(), -fDirection.GetZ());
+		Vector3 wanderDirection = bdirection.Normalize()* 3;
+
+		Vector3 targetcosa = transfrormcomponent->GetPosition() + wanderDirection;
+		Vector3 randomvec = Vector3(random360(), 0, random360());
+		targetcosa += randomvec * wanderRange;
+		
+		Vector3 targetOrientation = targetcosa + transfrormcomponent->GetRotation();
+		targetcosa = transfrormcomponent->GetPosition() + transfrormcomponent->GetRotation();
+		targetcosa += targetOrientation;
+
+		Vector3 lineal = Vector3(targetcosa.GetX() * aceleration, 0, targetcosa.GetZ() * aceleration);
+
+		rigidbodycomponent->SetVelocityLinear(lineal);
+	}
+	else actualtimer += dt;
+	
+}
---
+++ projects/MerodeoMovementComponent.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef MERODEOMOVEMENTCOMPONENT
+#define MERODEOMOVEMENTCOMPONENT
+#include "Component.h"
+using namespace VeryReal;
+namespace VeryReal {
+	class TransformComponent;
+	class RigidBodyComponent;
+}
+
+class MerodeoMovementComponent : public Component {
+private:
+	//cada cuanto cambia de direccion
+	int timertochange;
+	//cuanto tiempo llevo desde el cambio
+	int actualtimer;
+	//radio maximo entre el que puede cambiar
+	float wanderRange;
+	float aceleration;
+	//rigidbody de la entidad
+	RigidBodyComponent* rigidbodycomponent;
+	TransformComponent* transfrormcomponent;
+	int random360();
+
+public:
+	MerodeoMovementComponent();
+	virtual ~MerodeoMovementComponent();
+	void Update(const double& dt) override;
+	
+};
+#endif
+
