6da1da5294ea4d5f3782eb8c6abd3f1804d19d9d
CristinaMora <Crmora03@ucm.es>
2024-04-25 19:10:33 +0200

    Update All.bat

--- All.bat
+++ All.bat
@@ -8,6 +8,6 @@ cd ..\bin\
 
 xcopy Assets .\..\VeryReal\VeryRealSolucion\bin\Assets /E /Y
 
-cd ..\VeryReal\VeryRealSolucion\
-msbuild "VeryRealSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
-msbuild "VeryRealSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
+cd ..
+msbuild "OgremanSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
+msbuild "OgremanSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
