0c99867fe7d42c5c4471426bee8310955f8af695
CiscoGalvan <fragalva@ucm.es>
2024-04-30 15:57:43 +0200

    suelo

--- bin/LuaFiles/HouseScene.lua
+++ bin/LuaFiles/HouseScene.lua
@@ -5,7 +5,7 @@ Entities={
             {
                 name = "TransformComponent",
                 parameters = {  
-                    position = {0, 0, 10},
+                    position = {0, 4, 10},
                     rotation ={0, 0, 0},
                     scale = {1, 1, 1}                   
                 }
@@ -44,8 +44,8 @@ Entities={
                     shapeType = 1,
                     movementType = 0,
                     size = {2, 2, 2},
-                    mass = 1.0,
-                    friction = 0.0,
+                    mass = 10.0,
+                    friction = 0.5,
                     restitution = 0.0,
                     trigger = false,
                     mask = 2,
@@ -366,7 +366,6 @@ Entities={
 
                 }
             }
-           
         }
     }, 
     
@@ -422,8 +421,41 @@ Entities={
 
                 }
             }
+        }
+    },
+    {
+        name = "Suelo",
+        components = 
+        {
+            {
+                name = "TransformComponent",
+                parameters = {  
+                    a = 0,
+                    position = {0, -18, 0},
+                    rotation = {0, 0, 0},
+                    scale = {1, 1, 1}                   
+                }
+            },
             
-    
+            {
+                name = "ColliderComponent",
+                parameters = {
+                }
+            },
+            {
+                name = "RigidBodyComponent",
+                parameters = {
+                    shapeType = 1,
+                    movementType = 1,
+                    size = {200, 20, 200},
+                    mass = 1.0,
+                    friction =1.0,
+                    restitution=0.0,
+                    trigger = false,
+                    mask = 1,
+                    group = 2
+                }
+            },
         }
     }
    
