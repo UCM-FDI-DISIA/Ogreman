33c505c1431a9d57b23799a71a0aefd2ecf7cd19
AgusCDT <acastrod@ucm.es>
2024-05-10 00:27:05 +0200

    Update All.bat

--- All.bat
+++ All.bat
@@ -1,16 +1,13 @@
 ﻿::compilamos el motor
 cd VeryReal\VeryRealSolucion\dependencies\cMake
 call All.bat
+cd ..\..\
+msbuild "OgremanSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
+msbuild "OgremanSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
 cd VeryReal\VeryRealSolucion
 ::copiamos la carpeta entera de bin a bin de juego
 :: que quieres      donde lo quieres 
 xcopy bin .\..\..\bin\ /s /e
-
-
-cd ..\..\
-msbuild "OgremanSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
-msbuild "OgremanSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
-
 
 
 cd .\bin
